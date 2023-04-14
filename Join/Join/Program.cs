708a6463c2348cafda60a4f415f5dbe326834ee2
Maharoz <mahruj66@gmail.com>
2023-04-14 11:38:31 +0600

    Join implemented

---
+++ Join/Join/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Join
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//Joining Using Method Syntax
+			var joinUsingMethodSyntax = Employee.GetAllEmployees()
+				.Join(Address.GetAllAddresses(),
+				employee => employee.AddressId,
+				address => address.ID,
+				(employee, Address) => new
+				{
+					EmployeeName = employee.Name,
+					AddressLine = Address.AddressLine
+				}).ToList();
+
+
+			//Joining Using query syntax
+			var joinUsingQuerySyntax = (from emp in Employee.GetAllEmployees()
+										join add in Address.GetAllAddresses()
+										on emp.AddressId equals add.ID
+										select new
+										{
+											EmployeeName = emp.Name,
+											AddressLine = add.AddressLine
+										}).ToList();
+		}
+	}
+}
